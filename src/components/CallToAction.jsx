ba8eb0f31a95a4fb2a9049f3bbe67837f0f839a4
F4Bz3 <53076710+FabianWassermann@users.noreply.github.com>
2025-07-16 13:58:31 +0200

    Website Post

---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,46 @@
+import { Button } from '@/components/Button'
+import clsx from 'clsx'
+import { useRouter } from 'next/router'
+
+export function CallToAction({ className, text, buttonText, size, clickUrl }) {
+  let router = useRouter()
+
+  const theSize = size ?? 'medium'
+
+  const sizes = {
+    small: {
+      titleClassName: 'text-3xl sm:text-4xl',
+      buttonClassName: 'h-14 px-10',
+    },
+    medium: {
+      titleClassName: 'text-4xl sm:text-5xl',
+      buttonClassName: 'h-16 w-52',
+    },
+  }
+
+  return (
+    <div
+      className={clsx(
+        className,
+        'mt-28 lg:flex lg:items-center lg:justify-between'
+      )}
+    >
+      <h2
+        className={clsx(
+          sizes[theSize].titleClassName,
+          'my-0 max-w-2xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100'
+        )}
+      >
+        {text}
+      </h2>
+      <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:shrink-0">
+        <Button
+          className={clsx(sizes[theSize].buttonClassName, '!rounded-full')}
+          onClick={() => router.push(clickUrl)}
+        >
+          {buttonText}
+        </Button>
+      </div>
+    </div>
+  )
+}
